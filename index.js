3bec05b10063dc2029391e2ddbd323d04bc6b12d
Luis Fraga <luis5944@gmail.com>
2021-04-05 11:31:18 +0200

    add front

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express, { request, response } from "express";
 import cors from "cors";
 const app = express();
-app.use(express.json());
 
+//Middlewares
+app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 
 let notes = [
   {
